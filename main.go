18125bd74f2ce11de769c70661b19720b8a83612
sudodaisei <huyuyasuminotami@gmail.com>
2024-05-03 12:33:41 +0900

    軽微な修正。

--- main.go
+++ main.go
@@ -10,47 +10,49 @@ import (
 )
 
 func uploadFile(w http.ResponseWriter, r *http.Request) {
+	setupCORS(&w, r)
+	if r.Method == "OPTIONS" {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
 	// リクエストからファイルを解析
 	r.ParseMultipartForm(10 << 20) // 最大10MBのファイルまで扱う
-
-	// フォームから "file" というキーでファイルを取得
 	file, handler, err := r.FormFile("file")
 	if err != nil {
-		fmt.Println("Error Retrieving the File")
-		fmt.Println(err)
+		log.Printf("Error Retrieving the File: %v", err)
 		http.Error(w, "Error retrieving the file", http.StatusInternalServerError)
 		return
 	}
 	defer file.Close()
 
-	// MIMEタイプを確認
 	if !strings.HasPrefix(handler.Header.Get("Content-Type"), "image/") {
 		http.Error(w, "Only image uploads are allowed", http.StatusBadRequest)
 		return
 	}
 
-	// テンポラリディレクトリにファイルを保存
 	tempFile, err := os.CreateTemp("uploads", "upload-*.png")
 	if err != nil {
-		fmt.Println("Error Creating the File")
-		fmt.Println(err)
+		log.Printf("Error Creating the File: %v", err)
 		http.Error(w, "Failed to create a temporary file", http.StatusInternalServerError)
 		return
 	}
 	defer tempFile.Close()
 
-	// ファイルの内容を読み取って保存
 	fileBytes, err := io.ReadAll(file)
 	if err != nil {
-		fmt.Println("Error Reading the File")
-		fmt.Println(err)
+		log.Printf("Error Reading the File: %v", err)
 		http.Error(w, "Failed to read the file content", http.StatusInternalServerError)
 		return
 	}
 	tempFile.Write(fileBytes)
-
-	// 成功のレスポンスを返す
 	fmt.Fprintf(w, "Successfully Uploaded File: %s\n", handler.Filename)
+}
+
+func setupCORS(w *http.ResponseWriter, req *http.Request) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
 
 func main() {
@@ -63,7 +65,14 @@ func main() {
 	}
 
 	// ハンドラの設定
-	http.HandleFunc("/upload", uploadFile)
+	http.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) {
+		setupCORS(&w, r)
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		uploadFile(w, r)
+	})
 
 	// サーバの起動
 	log.Println("Server started on :8080")
